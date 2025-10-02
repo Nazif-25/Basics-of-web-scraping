e22eaa45e48b055a15aa31853879d7b4d4951d1f
Nazif-25 <139710237+Nazif-25@users.noreply.github.com>
2025-10-02 16:34:57 +0600

    Update quotes_spider.py

--- ScrapyTutorial/quotetutorial/quotetutorial/spiders/quotes_spider.py
+++ ScrapyTutorial/quotetutorial/quotetutorial/spiders/quotes_spider.py
@@ -5,26 +5,26 @@ from ..items import QuotetutorialItem
 class QuoteSpider(scrapy.Spider):
     # the "name" and "start_urls" variables are must have variables in any spider
     name = "quotes"
-    start_urls = [
-        "https://quotes.toscrape.com/"
-    ]
+    start_urls = ["https://quotes.toscrape.com/"]
 
     # response variable contains the source code of the website we're crawling
     def parse(self, response):
-        items = QuotetutorialItem()
-
         all_div_quotes = response.css('div.quote')
 
         for quote in all_div_quotes:
-            title = quote.css('span.text::text').extract()
-            author = quote.css('.author::text').extract()
-            tag = quote.css('.tag::text').extract()
+            # Create a new item for each loop iteration
+            items = QuotetutorialItem()
 
-            items["title"] = title
-            items["author"] = author
-            items["tag"] = tag
+            items["title"] = quote.css('span.text::text').get()
+            items["author"] = quote.css('.author::text').get()
+            items["tag"] = quote.css('.tag::text').getall()
 
             yield items
+
+        # Follow pagination links
+        next_page = response.css("li.next a::attr(href)").get()
+        if next_page is not None:
+            yield response.follow(next_page, callback=self.parse)
 
 
 
